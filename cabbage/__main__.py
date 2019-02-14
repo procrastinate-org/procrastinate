d828905e81de40962ce992cbaeebc6ef2b9f5d4d
Joachim Jablon <joachim.jablon@people-doc.com>
2019-02-14 16:42:36 +0100

    Try sleep tasks, for locks

--- cabbage/__main__.py
+++ cabbage/__main__.py
@@ -14,6 +14,13 @@ def sum(task_run, a, b):
 
 
 @task_manager.task(queue="sums")
+def sleep(task_run, i):
+    import time
+
+    time.sleep(i)
+
+
+@task_manager.task(queue="sums")
 def sum_plus_one(task_run, a, b):
     with open(f"{task_run.task.name}-{task_run.id}", "w") as f:
         f.write(f"{a + b + 1}")
@@ -26,10 +33,14 @@ def product(task_run, a, b):
 
 
 def client():
-    sum.defer(a=3, b=5)
-    sum.defer(a=5, b=7)
-    sum_plus_one.defer(a=4, b=7)
-    product.defer(a=2, b=8)
+    # sum.defer(a=3, b=5)
+    # sum.defer(a=5, b=7)
+    # sum.defer(a=5, b="a")
+    # sum_plus_one.defer(a=4, b=7)
+    # product.defer(a=2, b=8)
+    sleep.defer(lock="a", i=10)
+    sleep.defer(lock="a", i=12)
+    sleep.defer(lock="a", i=14)
 
 
 def main():
