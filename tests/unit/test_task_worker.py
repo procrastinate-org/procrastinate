379dc48944108606fd055cdbcbc12fb14337dfbf
Joachim Jablon <joachim.jablon@people-doc.com>
2019-04-21 16:22:37 +0200

    TaskWorker operates on a JobStore instead of the Manager

--- tests/unit/test_task_worker.py
+++ tests/unit/test_task_worker.py
@@ -1,11 +1,29 @@
 import pytest
 
-from cabbage import exceptions, postgres, task_worker, tasks
+from cabbage import exceptions, task_worker, tasks, testing, jobs
 
 
 @pytest.fixture
-def manager():
-    return tasks.TaskManager(connection=object())
+def manager(mocker):
+    return tasks.TaskManager(job_store=testing.InMemoryJobStore())
+
+
+@pytest.fixture
+def job_factory():
+    defaults = {
+        "id": 42,
+        "task_name": "bla",
+        "kwargs": {},
+        "lock": None,
+        "queue": "queue",
+    }
+
+    def factory(**kwargs):
+        final_kwargs = defaults.copy()
+        final_kwargs.update(kwargs)
+        return jobs.Job(**final_kwargs)
+
+    return factory
 
 
 def test_run(manager, mocker):
@@ -17,25 +35,24 @@ def test_run(manager, mocker):
                 self.stop(None, None)
             self.i += 1
 
-    listen_queue = mocker.patch("cabbage.postgres.listen_queue")
-    select = mocker.patch("select.select")
-
     worker = TestTaskWorker(task_manager=manager, queue="marsupilami")
 
     worker.run(timeout=42)
 
-    listen_queue.assert_called_with(connection=manager.connection, queue="marsupilami")
-    select.assert_called_with([manager.connection], [], [], 42)
+    manager.job_store.listening_queues == {"marsupilami"}
+    manager.job_store.waited == [42]
 
 
-def test_process_tasks(mocker, manager):
-    row1, row2, row3 = mocker.Mock(id=42), mocker.Mock(id=43), mocker.Mock(id=44)
-    mocker.patch("cabbage.postgres.get_tasks", return_value=[row1, row2, row3])
+def test_process_tasks(mocker, manager, job_factory):
+    job_1 = job_factory(id=42)
+    job_2 = job_factory(id=43)
+    job_3 = job_factory(id=44)
+    manager.job_store.jobs["queue"] = [job_1, job_2, job_3]
     worker = task_worker.Worker(manager, "queue")
 
     i = 0
 
-    def side_effect(task_row):
+    def side_effect(job):
         nonlocal i
         i += 1
         if i == 1:
@@ -51,23 +68,18 @@ def test_process_tasks(mocker, manager):
     call_task = mocker.patch(
         "cabbage.task_worker.Worker.run_task", side_effect=side_effect
     )
-    finish_task = mocker.patch("cabbage.postgres.finish_task")
-
     worker.process_tasks()
 
-    assert call_task.call_count == 3
-    assert finish_task.call_count == 3
-
     assert call_task.call_args_list == [
-        mocker.call(task_row=row1),
-        mocker.call(task_row=row2),
-        mocker.call(task_row=row3),
+        mocker.call(job=job_1),
+        mocker.call(job=job_2),
+        mocker.call(job=job_3),
     ]
 
-    assert finish_task.call_args_list == [
-        mocker.call(manager.connection, 42, "done"),
-        mocker.call(manager.connection, 43, "error"),
-        mocker.call(manager.connection, 44, "done"),
+    assert manager.job_store.finished_jobs == [
+        (job_1, jobs.Status.DONE),
+        (job_2, jobs.Status.ERROR),
+        (job_3, jobs.Status.DONE),
     ]
 
 
@@ -81,8 +93,8 @@ def test_run_task(manager):
 
     manager.tasks = {"job": task}
 
-    row = postgres.TaskRow(
-        id=16, args={"a": 9, "b": 3}, targeted_object="sherlock", task_type="job"
+    row = jobs.Job(
+        id=16, kwargs={"a": 9, "b": 3}, lock="sherlock", task_name="job", queue="yay"
     )
     worker = task_worker.Worker(manager, "yay")
     worker.run_task(row)
@@ -99,8 +111,8 @@ def test_run_task_error(manager):
 
     manager.tasks = {"job": task}
 
-    row = postgres.TaskRow(
-        id=16, args={"a": 9, "b": 3}, targeted_object="sherlock", task_type="job"
+    row = jobs.Job(
+        id=16, kwargs={"a": 9, "b": 3}, lock="sherlock", task_name="job", queue="yay"
     )
     worker = task_worker.Worker(manager, "yay")
     with pytest.raises(exceptions.TaskError):
@@ -108,8 +120,8 @@ def test_run_task_error(manager):
 
 
 def test_run_task_not_found(manager):
-    row = postgres.TaskRow(
-        id=16, args={"a": 9, "b": 3}, targeted_object="sherlock", task_type="job"
+    row = jobs.Job(
+        id=16, kwargs={"a": 9, "b": 3}, lock="sherlock", task_name="job", queue="yay"
     )
     worker = task_worker.Worker(manager, "yay")
     with pytest.raises(exceptions.TaskNotFound):
