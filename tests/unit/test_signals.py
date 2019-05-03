79b1aa00dafa74cb21eba42986c230e98056ddb3
Joachim Jablon <joachim.jablon@people-doc.com>
2019-05-03 15:08:43 +0200

    Disable signal handler when not in main thread

--- tests/unit/test_signals.py
+++ tests/unit/test_signals.py
@@ -19,3 +19,16 @@ def test_on_stop(one_signal, kill_own_pid):
 
     assert called == [True]
     assert signal.getsignal(one_signal) is before
+
+
+def test_do_nothing_on_secondary_thread(mocker):
+    mocker.patch("threading.main_thread", return_value=None)
+    signal = mocker.patch("signal.signal")
+
+    def stop():
+        pass
+
+    with signals.on_stop(callback=stop):
+        pass
+
+    signal.assert_not_called()
