64497a262d9d7c59c41e7eed6fab923043005626
Pierre Mourlanne <pmourlanne@gmail.com>
2019-07-19 14:51:23 +0200

    Added a function to get stalled jobs (#7)

--- cabbage/postgres.py
+++ cabbage/postgres.py
@@ -17,6 +17,17 @@ RETURNING id;
 select_jobs_sql = """
 SELECT id, task_name, lock, args, scheduled_at FROM cabbage_fetch_job(%(queue)s);
 """
+
+select_stalled_jobs_sql = """
+SELECT job.id, task_name, lock, args, scheduled_at, queue_name
+    FROM cabbage_jobs job
+    INNER JOIN cabbage_queues queue ON queue.id = job.queue_id
+WHERE status = 'doing'
+  AND started_at < NOW() - (%(nb_seconds)s || 'SECOND')::INTERVAL
+  AND (%(queue)s IS NULL OR queue_name = %(queue)s)
+  AND (%(task_name)s IS NULL OR task_name = %(task_name)s)
+"""
+
 finish_job_sql = """
 SELECT cabbage_finish_job(%(job_id)s, %(status)s);
 """
@@ -86,6 +97,31 @@ def get_jobs(
             }
 
 
+def get_stalled_jobs(
+    connection: psycopg2._psycopg.connection,
+    nb_seconds: int,
+    queue: str = None,
+    task_name: str = None,
+) -> Iterator[types.JSONDict]:
+    with connection.cursor(cursor_factory=RealDictCursor) as cursor:
+        with connection:
+            cursor.execute(
+                select_stalled_jobs_sql,
+                {"nb_seconds": nb_seconds, "queue": queue, "task_name": task_name},
+            )
+
+            rows = cursor.fetchall()
+            for row in rows:
+                yield {
+                    "id": row["id"],
+                    "lock": row["lock"],
+                    "task_name": row["task_name"],
+                    "task_kwargs": row["args"],
+                    "scheduled_at": row["scheduled_at"],
+                    "queue": row["queue_name"],
+                }
+
+
 def finish_job(
     connection: psycopg2._psycopg.connection, job_id: int, status: str
 ) -> None:
@@ -134,6 +170,18 @@ class PostgresJobStore(store.JobStore):
     def get_jobs(self, queue: str) -> Iterator[jobs.Job]:
         for job_dict in get_jobs(connection=self.connection, queue=queue):
             # Hard to tell mypy that every element of the dict is typed correctly
+            yield jobs.Job(job_store=self, **job_dict)  # type: ignore
+
+    def get_stalled_jobs(
+        self,
+        nb_seconds: int,
+        queue: Optional[str] = None,
+        task_name: Optional[str] = None,
+    ) -> Iterator[jobs.Job]:
+        job_dicts = get_stalled_jobs(
+            self.connection, nb_seconds=nb_seconds, queue=queue, task_name=task_name
+        )
+        for job_dict in job_dicts:
             yield jobs.Job(job_store=self, **job_dict)  # type: ignore
 
     def finish_job(self, job: jobs.Job, status: jobs.Status) -> None:
