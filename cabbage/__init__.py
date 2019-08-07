7dc8162bff1149e9ce8f8ebb27f4151e1456e6fa
Joachim Jablon <joachim.jablon@people-doc.com>
2019-08-07 08:17:16 +0200

    Add metadata in __init__

--- cabbage/__init__.py
+++ cabbage/__init__.py
@@ -1,4 +1,13 @@
 from cabbage.app import App
 from cabbage.postgres import PostgresJobStore
+from cabbage import metadata as _metadata_module
 
 __all__ = ["App", "PostgresJobStore"]
+
+
+_metadata = _metadata_module.extract_metadata()
+__author__ = _metadata["author"]
+__author_email__ = _metadata["email"]
+__copyright__ = _metadata["copyright"]
+__url__ = _metadata["url"]
+__version__ = _metadata["version"]
