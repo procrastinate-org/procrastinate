75a8beeafc29e2161346952c648316eb5ab63df2
Joachim Jablon <joachim.jablon@people-doc.com>
2019-08-09 18:22:55 +0200

    Add BaseJobStore in the official API to ease subclassing

--- cabbage/__init__.py
+++ cabbage/__init__.py
@@ -2,8 +2,15 @@ from cabbage import metadata as _metadata_module
 from cabbage.app import App
 from cabbage.postgres import PostgresJobStore
 from cabbage.retry import BaseRetryStrategy, RetryStrategy
+from cabbage.store import BaseJobStore
 
-__all__ = ["App", "PostgresJobStore", "RetryStrategy", "BaseRetryStrategy"]
+__all__ = [
+    "App",
+    "BaseJobStore",
+    "BaseRetryStrategy",
+    "PostgresJobStore",
+    "RetryStrategy",
+]
 
 
 _metadata = _metadata_module.extract_metadata()
