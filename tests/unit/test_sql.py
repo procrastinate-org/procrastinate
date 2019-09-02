0f2898c2a070abcda7464a4e3a3359151a82a3b3
Joachim Jablon <ewjoachim@gmail.com>
2019-09-02 22:42:26 +0200

    Create queries system

---
+++ tests/unit/test_sql.py
@@ -0,0 +1,34 @@
+from procrastinate import sql
+
+
+def test_parse_query_file():
+    assert (
+        sql.parse_query_file(
+            """
+-- Hello: This is ignored.
+yay
+
+-- query1 --
+Select bla
+
+-- query2 --
+-- Description
+
+SELECT blu
+
+-- query3 --
+-- multi-line
+-- description
+INSERT INTO blou VALUES(%(yay)s)
+    """
+        )
+        == {
+            "query1": "Select bla",
+            "query2": "SELECT blu",
+            "query3": "INSERT INTO blou VALUES(%(yay)s)",
+        }
+    )
+
+
+def test_get_queries():
+    assert {"insert_job", "fetch_job", "finish_job"} <= set(sql.get_queries())
