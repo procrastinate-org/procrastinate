06e16dcbb21fc379cc91b6348348b225607e58f5
Eliot Berriot <contact@eliotberriot.com>
2019-09-21 12:19:29 +0200

    Updated demo to show scheduler usage

--- procrastinate_demo/app.py
+++ procrastinate_demo/app.py
@@ -5,4 +5,26 @@ app = procrastinate.App(
         dsn="postgresql://postgres@localhost/procrastinate"
     ),
     import_paths=["procrastinate_demo.tasks"],
+    schedule=[
+        {
+            'job': 'procrastinate_demo.tasks.random_fail',
+            'cron': {
+                'minute': '*'
+            }
+        },
+        {
+            'job': 'procrastinate_demo.tasks.sum',
+            'kwargs': {'a': 1, 'b': 2},
+            'cron': {
+                'second': '*/3'
+            }
+        },
+        {
+            'job': 'procrastinate_demo.tasks.sleep',
+            'kwargs': {'i': 3},
+            'cron': {
+                'second': '*/5'
+            }
+        },
+    ]
 )
