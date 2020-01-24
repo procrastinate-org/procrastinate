429603d0399f0cc973f55a865ac69a30b5813377
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2020-01-24 11:02:07 +0100

    Change return type of checks.

--- procrastinate/healthchecks.py
+++ procrastinate/healthchecks.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from psycopg2 import Error
 from procrastinate import store, sql, utils
 from procrastinate.migration import Migrator
@@ -7,20 +9,20 @@ class HealthCheckRunner:
     def __init__(self, job_store: store.BaseJobStore):
         self.job_store = job_store
 
-    async def check_connection_async(self):
+    async def check_connection_async(self) -> Tuple[bool, str]:
         try:
             result = await self.job_store.execute_query_one(
                 query=sql.queries['check_connection'],
             )
-            return result['check']
+            return result['check'], "OK"
         except Error as e:
-            return False
+            return False, str(e)
 
-    async def check_db_version_async(self):
+    async def check_db_version_async(self) -> Tuple[bool, str]:
         try:
             result = await self.job_store.execute_query_one(
                 query=sql.queries['get_latest_version'],
             )
-            return result['version'] == Migrator.version
+            return result['version'] == Migrator.version, "OK"
         except Error as e:
-            return False
+            return False, str(e)
