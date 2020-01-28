1a24149fa37b747a11abe2029874fb78652c852f
Éric Lemoine <eric_lemoine@ultimatesoftware.com>
2020-01-28 18:19:33 +0100

    Don't set enable_json and enable_hstore to False
    Don't set enable_json and enable_hstore to False when calling aiopg.connect.
    
    aiopg does register_default_json(self._conn), so the registration of the
    json typecaster only applies to the connection being created.  And with
    Procrastinate creating its own connections there's no risk of overriding
    global application settings.

--- procrastinate/aiopg_connector.py
+++ procrastinate/aiopg_connector.py
@@ -16,11 +16,6 @@ def wrap_json(arguments: Dict[str, Any]):
 
 
 def get_connection(dsn="", **kwargs) -> Awaitable[aiopg.Connection]:
-    # tell aiopg not to register adapters for hstore & json by default, as
-    # those are registered at the module level and could overwrite previously
-    # defined adapters
-    kwargs.setdefault("enable_json", False)
-    kwargs.setdefault("enable_hstore", False)
     return aiopg.connect(dsn=dsn, **kwargs)
 
 
