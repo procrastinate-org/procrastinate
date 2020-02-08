04c978ca36dcbe5900396a0b09a3ffee63cd4090
Joachim Jablon <ewjoachim@gmail.com>
2020-02-08 10:58:20 +0100

    Adapt migrator

--- tests/unit/test_migration.py
+++ tests/unit/test_migration.py
@@ -7,15 +7,15 @@ def test_get_migration_queries(app):
     assert app.migrator.get_migration_queries().startswith("CREATE")
 
 
-def test_migrate(app, job_store):
-    job_store.reverse_queries = defaultdict(lambda: "migrate")
-    job_store.reverse_queries[
+def test_migrate(app, connector):
+    connector.reverse_queries = defaultdict(lambda: "migrate")
+    connector.reverse_queries[
         sql.queries["set_migration_version"]
     ] = "set_migration_version"
-    job_store.set_migration_version_run = lambda *a, **kw: None
+    connector.set_migration_version_run = lambda *a, **kw: None
     app.migrator.migrate()
 
-    assert app.job_store.queries == [
+    assert connector.queries == [
         ("migrate", {}),
         ("set_migration_version", {"version": app.migrator.version}),
     ]
