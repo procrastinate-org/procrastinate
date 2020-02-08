94acc327ba95f08906be3fdc57a5a5d65c9b0914
Joachim Jablon <ewjoachim@gmail.com>
2020-02-08 10:58:08 +0100

    Adapt healthchecks

--- procrastinate/healthchecks.py
+++ procrastinate/healthchecks.py
@@ -1,27 +1,28 @@
 from typing import Dict
 
-from procrastinate import jobs, sql, store, utils
+from procrastinate import connector as connector_module
+from procrastinate import jobs, sql, utils
 
 
 @utils.add_sync_api
 class HealthCheckRunner:
-    def __init__(self, job_store: store.BaseJobStore):
-        self.job_store = job_store
+    def __init__(self, connector: connector_module.BaseConnector):
+        self.connector = connector
 
     async def check_connection_async(self) -> bool:
-        result = await self.job_store.execute_query_one(
+        result = await self.connector.execute_query_one(
             query=sql.queries["check_connection"],
         )
         return result["check"]
 
     async def get_schema_version_async(self) -> str:
-        result = await self.job_store.execute_query_one(
+        result = await self.connector.execute_query_one(
             query=sql.queries["get_latest_version"],
         )
         return result["version"]
 
     async def get_status_count_async(self) -> Dict[jobs.Status, int]:
-        result = await self.job_store.execute_query_all(
+        result = await self.connector.execute_query_all(
             query=sql.queries["count_jobs_status"],
         )
         result_dict = {r["status"]: int(r["count"]) for r in result}
