0ad9de4cc10d2d6e2c3084f77eeb3907d946bfee
Joachim Jablon <joachim.jablon@people-doc.com>
2020-02-17 15:18:41 +0100

    Fix release for sdists

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
+import contextlib
+import os
+import pathlib
 import subprocess
 
 import setuptools
 
 
-def get_version():
+@contextlib.contextmanager
+def ensure_version():
+    version_filename = pathlib.Path(__file__).parent / "VERSION.txt"
+
+    # If installing from a sdist
     try:
-        return (
+        with open(version_filename) as f:
+            yield f.read().strip()
+        return
+    except IOError:
+        pass
+
+    # If running from the git repository
+    try:
+        version = (
             subprocess.check_output(["git", "describe"])
             .decode("utf-8")
             .strip()
@@ -13,7 +28,18 @@ def get_version():
             .replace("-", ".")
         )
     except subprocess.CalledProcessError:
+        # This might happen in some rare cases, like when running check-manifest.
+        # We'll update this to something better if it ever proves problematic.
+        yield
         return "0.0.0"
 
+    try:
+        with open(version_filename, "w") as f:
+            f.write(version)
+        yield version
+    finally:
+        os.remove(version_filename)
 
-setuptools.setup(version=get_version())
+
+with ensure_version() as version:
+    setuptools.setup(version=version)
