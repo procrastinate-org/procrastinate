504ac524be97796856a6c7eb9d337be863bd4bde
Joachim Jablon <joachim.jablon@people-doc.com>
2020-02-21 20:33:21 +0100

    Fix demo

--- procrastinate_demo/app.py
+++ procrastinate_demo/app.py
@@ -1,6 +1,6 @@
 import procrastinate
 
 app = procrastinate.App(
-    connector=procrastinate.PostgresConnector(),
+    connector=procrastinate.PostgresConnector.create_with_pool(),  # type: ignore
     import_paths=["procrastinate_demo.tasks"],
 )
