65fd3ef6f714504a8081adf6254d999bfb8caf92
Éric Lemoine <eric_lemoine@ultimatesoftware.com>
2020-02-21 22:43:34 +0100

    Add test for SchemaManager.get_version

--- tests/unit/test_schema.py
+++ tests/unit/test_schema.py
@@ -1,3 +1,4 @@
+import pathlib
 from collections import defaultdict
 
 from procrastinate import sql
@@ -6,6 +7,16 @@ from procrastinate.schema import SchemaManager
 
 def test_get_schema(app):
     assert app.schema_manager.get_schema().startswith("-- Schema version ")
+
+
+def test_get_version(mocker):
+    mock = mocker.patch("pathlib.Path.glob")
+    mock.return_value = [
+        pathlib.Path("/foo/bar/delta_1.0.8_test.sql"),
+        pathlib.Path("/foo/bar/delta_1.0.9_test.sql"),
+        pathlib.Path("/foo/bar/delta_1.0.10_test.sql"),
+    ]
+    assert SchemaManager.get_version() == "1.0.10"
 
 
 def test_apply_schema(app, connector):
