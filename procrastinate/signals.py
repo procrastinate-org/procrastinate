28ce19d65b8fef3e41aeda47f9973c9824051015
Joachim Jablon <ewjoachim@gmail.com>
2020-02-23 23:57:05 +0100

    Fix signal and asyncio

--- procrastinate/signals.py
+++ procrastinate/signals.py
@@ -1,17 +1,29 @@
+import asyncio
 import logging
 import signal
 import threading
+import types
 from contextlib import contextmanager
-from types import FrameType
 from typing import Callable
 
 logger = logging.getLogger(__name__)
 
-Signals = signal.Signals
+# A few things about signals and asyncio:
+# - https://docs.python.org/3/library/asyncio-eventloop.html#unix-signals
+# - If you don't use loop.add_signal_handler, your handler has no effect on the queue
+# - asyncio signal primitives don't give you a way to get the current handler for a
+#   signal
+# - If a signal has an asyncio handler, its "standard handler (installed through
+#   signal.signal) is ignored (you can't get it, you can't set one, it doesn't get
+#   called on the signal).
+# - asyncio handlers receive no parameter
+# This all mean we have to save the previous signals before installing ours, and for
+# uninstalling, we need to remove the async handler and only then re-add the normal
+# one. And hope that there was no previously set async handler.
 
 
 @contextmanager
-def on_stop(callback: Callable[[signal.Signals, FrameType], None]):
+def on_stop(callback: Callable[[], None]):
     if threading.current_thread() is not threading.main_thread():
         logger.debug(
             "Skipping signal handling, because this is not the main thread",
@@ -22,13 +34,21 @@ def on_stop(callback: Callable[[signal.Signals, FrameType], None]):
 
     sigint_handler = signal.getsignal(signal.SIGINT)
     sigterm_handler = signal.getsignal(signal.SIGTERM)
+    loop = asyncio.get_running_loop()
+
+    def uninstall_and_callback() -> None:
+        loop.remove_signal_handler(signal.SIGINT)
+        loop.remove_signal_handler(signal.SIGTERM)
+        signal.signal(signal.SIGINT, sigint_handler)
+        signal.signal(signal.SIGTERM, sigterm_handler)
+        callback()
 
     try:
         logger.debug(
             "Installing signal handler", extra={"action": "install_signal_handlers"}
         )
-        signal.signal(signal.SIGINT, callback)
-        signal.signal(signal.SIGTERM, callback)
+        loop.add_signal_handler(signal.SIGINT, uninstall_and_callback)
+        loop.add_signal_handler(signal.SIGTERM, uninstall_and_callback)
 
         yield
     finally:
@@ -36,5 +56,7 @@ def on_stop(callback: Callable[[signal.Signals, FrameType], None]):
             "Resetting previous signal handler",
             extra={"action": "uninstall_signal_handlers"},
         )
+        loop.remove_signal_handler(signal.SIGINT)
+        loop.remove_signal_handler(signal.SIGTERM)
         signal.signal(signal.SIGINT, sigint_handler)
         signal.signal(signal.SIGTERM, sigterm_handler)
--- tests/unit/test_signals.py
+++ tests/unit/test_signals.py
@@ -1,3 +1,4 @@
+import asyncio
 import signal
 
 import pytest
@@ -9,7 +10,7 @@ from procrastinate import signals
 def test_on_stop(one_signal, kill_own_pid):
     called = []
 
-    def stop(*args):
+    def stop():
         called.append(True)
 
     before = signal.getsignal(one_signal)
@@ -32,3 +33,31 @@ def test_do_nothing_on_secondary_thread(mocker):
         pass
 
     signal.assert_not_called()
+
+
+def test_on_stop_signal_twice(kill_own_pid):
+    with pytest.raises(KeyboardInterrupt):
+        with signals.on_stop(callback=lambda: None):
+            kill_own_pid(signal=signal.SIGINT)
+            kill_own_pid(signal=signal.SIGINT)
+
+
+@pytest.mark.asyncio
+def test_on_stop_work_with_asyncio(kill_own_pid):
+    # In this test, we want to make sure that interacting with synchronisation
+    # primitives from within a signal handler works.
+    event = asyncio.Event()
+
+    def stop():
+        event.set()
+
+    async def wait_and_kill():
+        await asyncio.sleep(0.001)
+        kill_own_pid()
+
+    try:
+        with signals.on_stop():
+            asyncio.create_task(wait_and_kill())
+            await asyncio.wait_for(event.wait(), timeout=0.01)
+    except asyncio.TimeoutError:
+        pytest.fail("Signal did not awake coroutine")
