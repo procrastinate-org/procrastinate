9aedbef16aaf3d0733fda95e12dbe726cc7bbfe1
Éric Lemoine <eric_lemoine@ultimatesoftware.com>
2020-02-28 19:58:57 +0100

    Remove SchemaManager.get_version

--- procrastinate/schema.py
+++ procrastinate/schema.py
@@ -1,14 +1,6 @@
-import pathlib
-import re
-
-import pkg_resources
 from importlib_resources import read_text
-
 from procrastinate import connector as connector_module
 from procrastinate import utils
-
-migrations_dir = pathlib.Path(__file__).parent / "sql" / "migrations"
-migration_script_pattern = re.compile(r"^delta_(\d+\.\d+\.\d+)_*\w*\.sql")
 
 
 @utils.add_sync_api
@@ -20,15 +12,6 @@ class SchemaManager:
     @staticmethod
     def get_schema() -> str:
         return read_text("procrastinate.sql", "schema.sql")
-
-    @staticmethod
-    def get_version():
-        matches = (
-            migration_script_pattern.match(script.name)
-            for script in migrations_dir.glob("*.sql")
-        )
-        versions = (match.group(1) for match in matches if match)
-        return str(max(versions, key=pkg_resources.parse_version))
 
     async def apply_schema_async(self) -> None:
         queries = self.get_schema()
--- tests/unit/test_schema.py
+++ tests/unit/test_schema.py
@@ -1,21 +1,8 @@
-import pathlib
 from collections import defaultdict
-
-from procrastinate.schema import SchemaManager
 
 
 def test_get_schema(app):
     assert app.schema_manager.get_schema().startswith("-- Schema version ")
-
-
-def test_get_version(mocker):
-    mock = mocker.patch("pathlib.Path.glob")
-    mock.return_value = [
-        pathlib.Path("/foo/bar/delta_1.0.8_test.sql"),
-        pathlib.Path("/foo/bar/delta_1.0.9_test.sql"),
-        pathlib.Path("/foo/bar/delta_1.0.10_test.sql"),
-    ]
-    assert SchemaManager.get_version() == "1.0.10"
 
 
 def test_apply_schema(app, connector):
