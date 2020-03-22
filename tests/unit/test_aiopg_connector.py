f00a773eb3d069fd62fa85d30cba06f8a7c6f6c4
Joachim Jablon <ewjoachim@gmail.com>
2020-03-22 15:00:54 +0100

    Keep unit test from using DB

--- tests/unit/test_aiopg_connector.py
+++ tests/unit/test_aiopg_connector.py
@@ -3,8 +3,12 @@ import pytest
 import procrastinate
 
 
-def test_app_deprecation(caplog):
+def test_app_deprecation(caplog, mocker):
+    connector = mocker.patch(
+        "procrastinate.aiopg_connector.PostgresConnector.create_with_pool"
+    )
     with pytest.deprecated_call():
         procrastinate.PostgresJobStore()
     assert caplog.records[0].levelname == "WARNING"
     assert caplog.records[0].message.startswith("Deprecation Warning")
+    connector.assert_called()
