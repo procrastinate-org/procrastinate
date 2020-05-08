1baadf9fdf465ee73bb47bae4afacbb4df21728f
Joachim Jablon <ewjoachim@gmail.com>
2020-05-08 19:15:07 +0200

    Add missing job context tests

--- tests/unit/test_job_context.py
+++ tests/unit/test_job_context.py
@@ -9,3 +9,36 @@ from procrastinate import job_context
 def test_queues_display(queues, result):
     context = job_context.JobContext(worker_queues=queues)
     assert context.queues_display == result
+
+
+def test_evolve():
+    context = job_context.JobContext(worker_name="a")
+    assert context.evolve(worker_name="b").worker_name == "b"
+
+
+def test_log_extra():
+    context = job_context.JobContext(
+        worker_name="a", worker_id=42, additional_context={"ha": "ho"}
+    )
+
+    assert context.log_extra(action="foo", bar="baz") == {
+        "action": "foo",
+        "bar": "baz",
+        "ha": "ho",
+        "worker": {"name": "a", "id": 42, "queues": None},
+    }
+
+
+def test_log_extra_job(job_factory):
+    job = job_factory()
+    context = job_context.JobContext(
+        worker_name="a", worker_id=42, job=job, additional_context={"ha": "ho"},
+    )
+
+    assert context.log_extra(action="foo", bar="baz") == {
+        "action": "foo",
+        "bar": "baz",
+        "ha": "ho",
+        "job": job.log_context(),
+        "worker": {"name": "a", "id": 42, "queues": None},
+    }
