70eded8bce7f959c93f059dfa4ef7afb87fae5af
Joachim Jablon <ewjoachim@gmail.com>
2020-05-16 11:06:28 +0200

    Add missing tests

--- tests/unit/test_job_context.py
+++ tests/unit/test_job_context.py
@@ -38,3 +38,29 @@ def test_log_extra_job(job_factory):
         "job": job.log_context(),
         "worker": {"name": "a", "id": 2, "queues": None},
     }
+
+
+def test_job_description_no_job(job_factory):
+    descr = job_context.JobContext(worker_name="a", worker_id=2).job_description(
+        current_timestamp=0
+    )
+    assert descr == "worker 2: no current job"
+
+
+def test_job_description_job_no_time(job_factory):
+    job = job_factory(task_name="some_task", id=12, task_kwargs={"a": "b"})
+    descr = job_context.JobContext(
+        worker_name="a", worker_id=2, job=job
+    ).job_description(current_timestamp=0)
+    assert descr == "worker 2: some_task[12](a='b')"
+
+
+def test_job_description_job_time(job_factory):
+    job = job_factory(task_name="some_task", id=12, task_kwargs={"a": "b"})
+    descr = job_context.JobContext(
+        worker_name="a",
+        worker_id=2,
+        job=job,
+        additional_context={"start_timestamp": 20.0},
+    ).job_description(current_timestamp=30.0)
+    assert descr == "worker 2: some_task[12](a='b') (started 10.000 s ago)"
