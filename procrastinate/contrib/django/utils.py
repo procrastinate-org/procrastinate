d2d559ef7486c9113f19a41fadcd270753aabe95
Agate <me@agate.blue>
2020-07-18 19:21:43 +0200

    Linting

--- procrastinate/contrib/django/utils.py
+++ procrastinate/contrib/django/utils.py
@@ -2,8 +2,6 @@ import pathlib
 
 
 def get_sql(name: str) -> str:
-    sql_path = migration_path = (
-        (pathlib.Path(__file__).parent.parent.parent) / "sql" / "migrations"
-    )
-    with open(migration_path / name, "rb") as f:
+    sql_path = (pathlib.Path(__file__).parent.parent.parent) / "sql" / "migrations"
+    with open(sql_path / name, "rb") as f:
         return f.read().decode()
