13af0be905b649a581197e14de6ef7300802a706
Thomas Perrot <thomas_perrot@ultimatesoftware.com>
2020-07-31 14:16:44 +0200

    Context manager: fix procrastinate demo

--- procrastinate_demo/app.py
+++ procrastinate_demo/app.py
@@ -5,3 +5,4 @@ import_paths = ["procrastinate_demo.tasks"]
 connector_class = procrastinate.Psycopg2Connector
 
 app = procrastinate.App(connector=connector_class(), import_paths=import_paths)
+app.open()
