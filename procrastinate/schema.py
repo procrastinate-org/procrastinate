646781d519297c0cedab6b9c3829403037e2c5d4
nils <nils.jonalik@rwth-aachen.de>
2020-12-31 12:41:32 +0100

    add async schmea apply

--- procrastinate/schema.py
+++ procrastinate/schema.py
@@ -23,3 +23,7 @@ class SchemaManager:
     def apply_schema(self) -> None:
         queries = self.get_schema()
         self.connector.execute_query(query=queries)
+
+    async def apply_schema_async(self) -> None:
+        queries = self.get_schema()
+        await self.connector.execute_query_async(query=queries)
