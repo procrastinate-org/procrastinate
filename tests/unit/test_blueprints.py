8fe07cb66b22f7c1d9e4f97707a84dfb4ce91be3
Tom Marks <thomas.o.marks@gmail.com>
2021-07-29 19:58:03 -0400

    test: Add implicit task creation test for blueprints

--- tests/unit/test_blueprints.py
+++ tests/unit/test_blueprints.py
@@ -26,3 +26,21 @@ def test_app_task_explicit(app, mocker):
     assert app.tasks["foobar"].pass_context is True
     assert app.tasks["foobar"] is wrapped
     assert app.tasks["foobar"].func is wrapped.__wrapped__
+
+
+def test_app_task_implicit(app):
+    bp = blueprints.Blueprint()
+
+    @bp.task
+    def wrapped():
+        return "foo"
+
+    app.register_blueprint(bp)
+
+    registered_task = app.tasks["tests.unit.test_blueprints.wrapped"]
+
+    assert "foo" == wrapped()
+    assert "tests.unit.test_blueprints.wrapped" == registered_task.name
+    assert "default" == registered_task.queue
+    assert registered_task is wrapped
+    assert registered_task.func is wrapped.__wrapped__
