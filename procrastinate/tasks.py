4c1908fc24c833234356d0ce75609cfec9f4a8b9
Joachim Jablon <ewjoachim@gmail.com>
2022-04-20 09:45:38 +0200

    Fix markdown

--- procrastinate/tasks.py
+++ procrastinate/tasks.py
@@ -160,7 +160,7 @@ class Task:
             A datetime before which the job should not be launched (incompatible with
             schedule_in)
         schedule_in :
-            A dict with kwargs for a python timedelta, for example `{'minutes': 5}`.
+            A dict with kwargs for a python timedelta, for example ``{'minutes': 5}``.
             Converted to schedule_at internally. See `python timedelta documentation
             <https://docs.python.org/3/library/datetime.html#timedelta-objects>`__
             (incompatible with schedule_at)
