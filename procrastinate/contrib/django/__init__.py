69bc2d5884ce597c821789a5396c3ece0a90ae4e
Joachim Jablon <ewjoachim@gmail.com>
2024-01-21 02:57:05 +0100

    django: Add models & admin

--- procrastinate/contrib/django/__init__.py
+++ procrastinate/contrib/django/__init__.py
@@ -4,12 +4,17 @@ from typing import cast
 
 import procrastinate
 
+from .router import ProcrastinateReadOnlyRouter
 from .utils import connector_params
 
-__all__ = ["connector_params"]
+__all__ = [
+    "app",
+    "connector_params",
+    "ProcrastinateReadOnlyRouter",
+]
 default_app_config = "procrastinate.contrib.django.apps.ProcrastinateConfig"
 
 # Before the Django app is ready, we're defining the app as a blueprint so
-# that tasks can be registered. The app will be initialized in the
+# that tasks can be registered. The real app will be initialized in the
 # ProcrastinateConfig.ready() method.
 app: procrastinate.App = cast(procrastinate.App, procrastinate.Blueprint())
