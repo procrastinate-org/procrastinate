1b5a651e802a6627e5c623a9ca5ca736163e1b11
Katlyn Lorimer <katlyn@hydrolix.io>
2024-08-07 10:04:01 -0800

    test: add cancelled and aborted jobs to remove_old_jobs

--- tests/unit/test_builtin_tasks.py
+++ tests/unit/test_builtin_tasks.py
@@ -5,11 +5,20 @@ from procrastinate import builtin_tasks, job_context
 
 async def test_remove_old_jobs(app):
     await builtin_tasks.remove_old_jobs(
-        job_context.JobContext(app=app), max_hours=2, queue="queue_a", remove_error=True
+        job_context.JobContext(app=app),
+        max_hours=2,
+        queue="queue_a",
+        remove_error=True,
+        remove_cancelled=True,
+        remove_aborted=True,
     )
     assert app.connector.queries == [
         (
             "delete_old_jobs",
-            {"nb_hours": 2, "queue": "queue_a", "statuses": ["succeeded", "failed"]},
+            {
+                "nb_hours": 2,
+                "queue": "queue_a",
+                "statuses": ["succeeded", "failed", "cancelled", "aborted"],
+            },
         )
     ]
