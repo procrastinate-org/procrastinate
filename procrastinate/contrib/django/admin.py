de8593c5500f205caa2ab9d2decaa178ecb846bb
Joachim Jablon <ewjoachim@gmail.com>
2024-08-08 23:36:18 +0200

    Continue improving the admin with small details

--- procrastinate/contrib/django/admin.py
+++ procrastinate/contrib/django/admin.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+import json
+
 from django.contrib import admin
 from django.template.loader import render_to_string
+from django.utils import timezone
+from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
 from . import models
@@ -34,21 +38,23 @@ class ProcrastinateJobAdmin(admin.ModelAdmin):
         "priority",
         "scheduled_at",
         "attempts",
-        "summary",
     ]
     list_display = [
         "pk",
         "short_task_name",
         "pretty_args",
         "pretty_status",
+        "summary",
+    ]
+    list_filter = [
+        "status",
         "queue_name",
+        "task_name",
         "lock",
         "queueing_lock",
-        "priority",
         "scheduled_at",
-        "attempts",
+        "priority",
     ]
-    list_filter = ["status"]
     inlines = [ProcrastinateEventInline]
 
     def get_readonly_fields(
@@ -74,21 +80,38 @@ class ProcrastinateJobAdmin(admin.ModelAdmin):
     @admin.display(description="Status")
     def pretty_status(self, instance: models.ProcrastinateJob) -> str:
         emoji = JOB_STATUS_EMOJI_MAPPING.get(instance.status, "")
-        return f"{emoji} ({instance.status})"
+        return f"{emoji} {instance.status.title()}"
 
     @admin.display(description="Task Name")
     def short_task_name(self, instance: models.ProcrastinateJob) -> str:
         *modules, name = instance.task_name.split(".")
-        return ".".join(m[0] for m in modules) + f".{name}"
+        return format_html(
+            "<span title='{task_name}'>{name}</span>",
+            task_name=instance.task_name,
+            name=".".join(m[0] for m in modules) + f".{name}",
+        )
 
     @admin.display(description="Args")
     def pretty_args(self, instance: models.ProcrastinateJob) -> str:
-        return mark_safe(f"<pre>{instance.args}</pre>")
+        indent = 2 if len(instance.args) > 1 or len(str(instance.args)) > 30 else None
+        pretty_json = json.dumps(instance.args, indent=indent)
+        if len(pretty_json) > 2000:
+            pretty_json = pretty_json[:2000] + "..."
+        return format_html(
+            '<pre style="margin: 0">{pretty_json}</pre>', pretty_json=pretty_json
+        )
 
     @admin.display(description="Summary")
     def summary(self, instance: models.ProcrastinateJob) -> str:
         if last_event := instance.procrastinateevent_set.latest():  # type: ignore[attr-defined]
             return mark_safe(
-                render_to_string("summary.html", {"last_event": last_event}).strip()
+                render_to_string(
+                    "procrastinate/admin/summary.html",
+                    {
+                        "last_event": last_event,
+                        "job": instance,
+                        "now": timezone.now(),
+                    },
+                ).strip()
             )
         return ""
