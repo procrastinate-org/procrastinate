355da011ddbe29c75bc2c9dee16a7248e8119628
Joachim Jablon <ewjoachim@gmail.com>
2024-12-01 20:12:35 +0100

    Try simplifying the noxfile

--- noxfile.py
+++ noxfile.py
@@ -39,13 +39,8 @@ def get_pre_migration(latest_tag: packaging.version.Version) -> str:
 @nox.session
 def current_version_with_post_migration(session: nox.Session):
     session.install("poetry")
-    session.run("poetry", "install", "--all-extras")
-    session.run(
-        "poetry",
-        "run",
-        "pytest",
-        *session.posargs,
-    )
+    session.run("poetry", "install", "--all-extras", external=True)
+    session.run("poetry", "run", "pytest", *session.posargs, external=True)
 
 
 @nox.session
@@ -53,7 +48,6 @@ def current_version_without_post_migration(session: nox.Session):
     latest_tag = fetch_latest_tag(session)
     pre_migration = get_pre_migration(latest_tag)
 
-    session.install("poetry")
     session.run("poetry", "install", "--all-extras")
     session.run(
         "poetry",
@@ -62,6 +56,7 @@ def current_version_without_post_migration(session: nox.Session):
         f"--migrate-until={pre_migration}",
         "./tests/acceptance",
         *session.posargs,
+        external=True,
     )
 
 
@@ -71,33 +66,26 @@ def stable_version_without_post_migration(session: nox.Session):
     pre_migration = get_pre_migration(latest_tag)
 
     with tempfile.TemporaryDirectory() as temp_dir:
+        session.chdir(temp_dir)
+
         temp_path = pathlib.Path(temp_dir)
         base_path = pathlib.Path(__file__).parent
+
+        # Install test dependencies and copy tests
         shutil.copytree(base_path / "tests", temp_path / "tests")
         shutil.copy(base_path / "pyproject.toml", temp_path / "pyproject.toml")
         shutil.copy(base_path / "poetry.lock", temp_path / "poetry.lock")
-        session.chdir(temp_dir)
+        session.run("poetry", "install", "--with", "test", "--no-root", external=True)
 
-        session.install("poetry")
-        session.install("poetry-plugin-export")
-        session.run(
-            "poetry",
-            "export",
-            "--output",
-            "requirements.txt",
-            "--with",
-            "test",
-            "--all-extras",
-            "--without-hashes",
-            env={"POETRY_WARNINGS_EXPORT": "false"},
-        )
-        session.install("-r", "requirements.txt")
+        # Install latest procrastinate from PyPI
         session.install("procrastinate")
+
         session.run(
             "pytest",
             f"--migrate-until={pre_migration}",
             f"--latest-version={latest_tag}",
             "./tests/acceptance",
             *session.posargs,
+            # This is necessary for pytest-django, due to not installing the project
             env={"PYTHONPATH": temp_dir},
         )
