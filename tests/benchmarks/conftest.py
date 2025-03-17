8be334bd3b5d8a775a19d0b62030fd48275d9cf3
Kai Schlamp <kai.schlamp@gmail.com>
2025-03-17 21:44:13 +0000

    Add some very simple benchmarks

---
+++ tests/benchmarks/conftest.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import asyncio
+
+import pytest
+
+
+@pytest.fixture
+def aio_benchmark(benchmark):
+    def _wrapper(func, *args, **kwargs):
+        if asyncio.iscoroutinefunction(func):
+            event_loop = asyncio.get_event_loop()
+
+            @benchmark
+            def _():
+                return event_loop.run_until_complete(func(*args, **kwargs))
+        else:
+            benchmark(func, *args, **kwargs)
+
+    return _wrapper
