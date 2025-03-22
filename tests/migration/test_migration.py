8ed4ba62b92b6c0115e854f412c419efa0933137
Joachim Jablon <ewjoachim@gmail.com>
2025-03-22 12:26:09 +0100

    Add tests for the names of migration files

--- tests/migration/test_migration.py
+++ tests/migration/test_migration.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 
 import contextlib
 import pathlib
+import subprocess
 import warnings
 
+import packaging.version
 import pytest
 from django.core import management
 from django.db import connection
@@ -98,3 +100,103 @@ def test_django_migrations_run_properly(django_db):
 
 def test_no_missing_django_migration(django_db):
     management.call_command("makemigrations", "procrastinate", dry_run=True, check=True)
+
+
+@pytest.fixture(scope="module")
+def latest_version() -> packaging.version.Version:
+    try:
+        subprocess.check_call(["git", "fetch", "--tags"])
+        out = subprocess.check_output(["git", "tag", "--list"], text=True)
+    except subprocess.CalledProcessError as exc:
+        raise ValueError("Cannot fetch latest tag") from exc
+
+    return max(packaging.version.Version(tag) for tag in out.splitlines())
+
+
+migration_files = sorted(
+    (pathlib.Path(__file__).parents[2] / "procrastinate" / "sql" / "migrations").glob(
+        "*.sql"
+    )
+)
+
+
+@pytest.fixture(scope="module")
+def new_migrations(latest_version) -> set[pathlib.Path]:
+    # git diff latest_version..HEAD --name-only --diff-filter=A
+
+    try:
+        out = subprocess.check_output(
+            [
+                "git",
+                "diff",
+                f"{latest_version}..HEAD",
+                "--name-only",
+                "--diff-filter=A",
+                "procrastinate/sql/migrations",
+            ],
+            text=True,
+            stderr=subprocess.PIPE,
+        )
+    except subprocess.CalledProcessError as exc:
+        raise ValueError(f"Cannot fetch new migrations: {exc.stderr}") from exc
+
+    return {pathlib.Path(path) for path in out.splitlines()}
+
+
+@pytest.mark.parametrize(
+    "migration", [pytest.param(m, id=m.name) for m in migration_files]
+)
+def test_migration_properly_named(
+    migration: pathlib.Path,
+    latest_version: packaging.version.Version,
+    new_migrations: set[pathlib.Path],
+):
+    # migration is:
+    # pathlib.Path("..." / "03.00.00_01_pre_cancel_notification.sql")
+
+    migration_name_parts = migration.stem.split("_", 3)
+    version_str, index_str, *pre_post_name = migration_name_parts
+
+    mig_version = packaging.version.Version(version_str)
+
+    next_minor = packaging.version.Version(
+        f"{latest_version.major}.{latest_version.minor + 1}.0"
+    )
+
+    if migration.name in {m.name for m in new_migrations}:
+        assert mig_version == next_minor, (
+            f"New migration {migration.name} should be named with {next_minor} but is {mig_version}"
+        )
+    else:
+        assert mig_version <= latest_version, (
+            f"Migration {migration.name} should be named with at most {latest_version} but is {mig_version}"
+        )
+
+    # All migrations before 3.0.0 are pre migrations
+    if mig_version < packaging.version.Version("3.0.0"):
+        pre_post = "pre"
+        name = pre_post_name[0]
+    else:
+        pre_post, name = pre_post_name
+
+    index = int(index_str)
+    if pre_post == "pre":
+        assert 1 <= index < 50, (
+            f"Pre migration {migration.name} should have an index between 1 and 49, but is {index}"
+        )
+    elif pre_post == "post":
+        assert 50 <= index < 100, (
+            f"Post migration {migration.name} should have an index of at least 50, but is {index}"
+        )
+    else:
+        assert False, f"Invalid migration name: expecting 'pre' or 'post': {pre_post}"
+
+    assert name == name.lower(), (
+        f"Migration {migration.name} should be lower case, but is {name}"
+    )
+    assert "-" not in name, (
+        f"Migration {migration.name} should not contain dashes, but is {name}"
+    )
+    assert " " not in name, (
+        f"Migration {migration.name} should not contain spaces, but is {name}"
+    )
