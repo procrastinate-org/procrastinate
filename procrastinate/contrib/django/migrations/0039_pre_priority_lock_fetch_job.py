7159be4d62cee88ad5db5a16efcf7e435cd08f15
Joachim Jablon <ewjoachim@gmail.com>
2025-07-17 01:15:57 +0200

    Rename migrations

--- procrastinate/contrib/django/migrations/0039_pre_priority_lock_fetch_job.py
+++ procrastinate/contrib/django/migrations/0039_pre_priority_lock_fetch_job.py
@@ -8,7 +8,7 @@ from .. import migrations_utils
 class Migration(migrations.Migration):
     operations = [
         migrations_utils.RunProcrastinateSQL(
-            name="03.03.00_01_pre_priority_lock_fetch_job.sql"
+            name="03.04.00_01_pre_priority_lock_fetch_job.sql"
         ),
     ]
     name = "0039_pre_priority_lock_fetch_job"
